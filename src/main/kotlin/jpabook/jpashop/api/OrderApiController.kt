6a9866378d86310fd5667801c40522eefbacf508
Seokhyun Bae <bus1029@gmail.com>
2022-05-19 18:55:22 +0900

    feat: 주문 컬렉션 조회 API 컨트롤러 작성 및 문제점 파악
    - v1, v2의 경우 다음과 같은 문제점이 존재한다.
    --- v1: 지연로딩 때문에 위해 강제로 데이터를 긁어옴
    --- v1: 엔티티를 그대로 외부에 노출
    --- v2: DTO를 노출했지만 지연로딩으로 인해 1+N 문제가 발생
    (Order 1번 / Member, Address N번 / orderItem N번 / item N번)

---
+++ src/main/kotlin/jpabook/jpashop/api/OrderApiController.kt
@@ -0,0 +1,69 @@
+package jpabook.jpashop.api
+
+import jpabook.jpashop.domain.Address
+import jpabook.jpashop.domain.Order
+import jpabook.jpashop.domain.OrderItem
+import jpabook.jpashop.domain.OrderStatus
+import jpabook.jpashop.repository.OrderRepository
+import jpabook.jpashop.repository.OrderSearch
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RestController
+class OrderApiController(
+  private val orderRepository: OrderRepository
+) {
+  @GetMapping("/api/v1/orders")
+  fun ordersV1(): List<Order> {
+    val all = orderRepository.findAllByString(OrderSearch())
+    for (order in all) {
+      // 강제 지연로딩 초기화
+      order.member?.name
+      order.delivery?.address
+      val orderItems = order.getOrderItems()
+      orderItems.forEach { orderItem -> orderItem.item?.name }
+    }
+    return all
+  }
+
+  @GetMapping("/api/v2/orders")
+  fun ordersV2(): List<OrderDto> {
+    val orders = orderRepository.findAllByString(OrderSearch())
+    return orders
+      .map {o -> OrderDto(o)}
+      .toList()
+  }
+
+  class OrderDto() {
+    var orderId: Long? = null
+    var name: String = ""
+    var orderDate: LocalDateTime? = null
+    var orderStatus: OrderStatus? = null
+    var address: Address? = null
+    var orderItems: List<OrderItemDto>? = null
+
+    constructor(order: Order) : this() {
+      orderId = order.id
+      name = order.member?.name ?: ""
+      orderDate = order.orderDate
+      orderStatus = order.status
+      address = order.delivery?.address
+      orderItems = order.getOrderItems()
+        .map { o -> OrderItemDto(o) }
+        .toList()
+    }
+  }
+
+  class OrderItemDto() {
+    var itemName: String = ""
+    var orderPrice = 0
+    var count = 0
+
+    constructor(orderItem: OrderItem) : this() {
+      itemName = orderItem.item?.name ?: ""
+      orderPrice = orderItem.orderPrice
+      count = orderItem.count
+    }
+  }
+}
